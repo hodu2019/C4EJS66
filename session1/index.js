8f4975f5a420f141bb6a6deff0ca932feacfec0e
hoangduc221997 <duongtom.221@gmail.com>
2019-12-22 23:07:27 +0700

    .

---
+++ session1/index.js
@@ -0,0 +1,29 @@
+// console.log("Hello", name)
+// Code chạy từ trên xuống dưới
+// Trong 1 thời điểm thì biến chỉ lưu giữ 1 giá trị thôi
+
+// let name = "Son Tung"
+// name = "Dam Vinh Hung"
+// let name = 10;
+// // let number = "6";
+// let number = 10;
+// console.log(name + number);
+
+// let userInput = prompt()
+// console.log(userInput)
+
+// let x = Number(prompt());
+// let y = Number(prompt());
+
+// // var
+
+// // const
+// console.log(x+y);
+// const number = 10;
+
+for(let i = 0; i < 4; i += 2){
+    console.log("hehe")
+} 
+
+
+
